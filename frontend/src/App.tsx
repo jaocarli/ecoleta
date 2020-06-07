0756786fddb2cb745f3188eeefad26770b170fbd
João Carli <jthorlief@gmail.com>
2020-06-07 00:33:01 -0300

    finished frontend / backend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-
-import Header from './Header';
+import Routes from './routes'
 
 function App() {
-  const counter = useState(0);
-
-  function handleButtonClick() {
-    counter++;
-    console.log(counter);
-  }
-
   return (
-        (
-          <div>
-            <Header title="Hello World" />
-
-            <h1>{counter}</h1>
-            <button type="button" onClick={handleButtonClick}>aumentar</button>
-          </div>
-        )
+    <Routes />
   );
 }
 
